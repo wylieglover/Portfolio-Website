6a529b0127f4cda153f3d37960ae656544fd9e3a
WylieGlover <rg11445@georgiasouthern.edu>
2023-12-25 16:21:16 -0500

    added resume

--- portfolio-website/src/pages/Resume.js
+++ portfolio-website/src/pages/Resume.js
@@ -1,41 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { saveAs } from "file-saver";
 import "./Resume.css";
+import resume1 from "../images/resume1.png";
+import resume2 from "../images/resume2.png";
+import resumePDF from "../images/resume.pdf";
 
 const Resume = () => {
+
+    const [isFirstImage, setIsFirstImage] = useState(true);
+
+    const handleClick = () => {
+        setIsFirstImage(prevState => !prevState);
+    };
+
+    const handleDownload = () => {
+        saveAs(resumePDF, "resume.pdf");
+    };
+
     return (
         <div className="resume">
-            <div className="resume-header">
-                <div className="resume-header-name">
-                    <h1>Richard "Wylie" Glover</h1>
+            <div className="left-column">
+                <div className="resume-header">
+                    <div className="resume-header-name">
+                        <h1>Richard "Wylie" Glover</h1>
+                    </div>
+                    <div className="resume-header-email">
+                        <h3>rwylieg@gmail.com</h3>
+                        <h3>(770) 508-7634</h3>
+                    </div>
                 </div>
-                <div className="resume-header-email">
-                    <h3>rwylieg@gmail.com</h3>
-                    <h3>(770) 508-7634</h3>
+                <div className="resume-content">
+                    <div className="skills">
+                        <h2>Skills</h2>
+                        <ul>
+                            <li>C/C++</li>
+                            <li>Java</li>
+                            <li>Python</li>
+                            <li>JavaScript</li>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                        </ul>
+                    </div>
+                    <div className="technologies">
+                        <h2>Platforms</h2>
+                        <ul>
+                            <li>Django</li>
+                            <li>React.js</li>
+                            <li>Selenium</li>
+                            <li>Linux</li>
+                            <li>Ruby on Rails</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-            <div className="resume-content">
-                <div className="skills">
-                    <h2>Skills</h2>
-                    <ul>
-                        <li>C/C++</li>
-                        <li>Java</li>
-                        <li>Python</li>
-                        <li>JavaScript</li>
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <li>Problem-solving</li>
-                        <li>Team collaboration</li>
-                    </ul>
-                </div>
-                <div className="technologies">
-                    <h2>Platforms and Frameworks</h2>
-                    <ul>
-                        <li>Django</li>
-                        <li>React.js</li>
-                        <li>Selenium</li>
-                        <li>Linux</li>
-                        <li>Ruby on Rails</li>
-                    </ul>
+            <div className="right-column">
+                <div className="image-container">
+                    <div className="download-button" onClick={handleDownload}>
+                        DOWNLOAD PDF
+                    </div>
+                    <img
+                        src={isFirstImage ? resume1 : resume2}
+                        alt={isFirstImage ? 'First Image' : 'Second Image'}
+                        onClick={handleClick}
+                        className="swap-image"
+                    />
                 </div>
             </div>
         </div>
