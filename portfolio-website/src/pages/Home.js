75a24cc2cdfdab3ba9bc06af4b31c5d4fa92a005
WylieGlover <rg11445@georgiasouthern.edu>
2023-12-25 00:16:54 -0500

    added email icon and image hover functionality

--- portfolio-website/src/pages/Home.js
+++ portfolio-website/src/pages/Home.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from 'react';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import "./Home.css"
+import wylie_image from "./wylie.jpg";
 
 const Home = () => {
-    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    const [imagePosition, setImagePosition] = useState({ top: 0, left: 0 });
+
+    const handleMouseMove = (e) => {
+        setImagePosition({ top: e.clientY, left: e.clientX });
+    };
+
     return (
         <div className="center-content">
             <div className="name">
-                <span className="bold-text">Hello, I'm Wylie Glover!</span>
+                <span className="bold-text">Hello, I'm </span>
+                <span className="hoverable-text" onMouseMove={handleMouseMove}>Wylie Glover!</span>
+                <img src={wylie_image} alt="Profile" id="cursor-image" className="circular-image" style={{ top: imagePosition.top, left: imagePosition.left }} />
             </div>
             <div className="bio">
-                Aspiring Software Engineer | Recent graduate from Georgia Southern University
+                Software Engineer | Recent graduate from Georgia Southern University
                 <br />
                 Passionate about crafting innovative solutions through code
             </div>
@@ -21,6 +30,9 @@ const Home = () => {
                 </a>
                 <a href="https://www.linkedin.com/in/wylieglover" target="_blank" rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+                <a href="mailto:rwylieg@gmai.com" target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faEnvelope} style={{ fontSize: '43px' }} />
                 </a>
             </div>
         </div>
