d85bdb85f9c66814a4d5a53f764ed70c2d0f4dda
WylieGlover <rg11445@georgiasouthern.edu>
2024-01-01 21:30:02 -0500

    added resume.pdf page

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,29 +1,37 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-
 import Home from "./pages/Home";
 import Resume from "./pages/Resume";
 import Projects from "./pages/Projects";
+import ResumePDF from "./components/ResumePDF";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/projects" element={<Projects />} />
-          <Route path="/resume" element={<Resume />} />
-        </Routes>
+        <AppRoutes />
       </Router>
     </div>
   );
 }
+
+const AppRoutes = () => {
+  const location = useLocation();
+  const isResumePDF = location.pathname === '/resume.pdf';
+
+  return (
+    <>
+      {!isResumePDF && <Navbar />} {/* Render Navbar unless it's /resume.pdf */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/resume.pdf" element={<ResumePDF />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
