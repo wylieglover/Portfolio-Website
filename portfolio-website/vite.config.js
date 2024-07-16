716485a77309e789f475ef89984ab282099bc33b
wylieglover <rg11445@georgiasouthern.edu>
2024-07-16 13:47:01 -0400

    trying out vite

--- portfolio-website/vite.config.js
+++ portfolio-website/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Listen on all addresses, including external ones
+    port: 3000, // Ensure the port matches the one exposed in Dockerfile
+    strictPort: true, // Fail if the port is already in use
+  },
 })
