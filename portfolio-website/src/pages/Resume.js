cfe1ac5769ee75056f5ef670e2fb4eb88c731353
wylieglover <rg11445@georgiasouthern.edu>
2024-07-16 23:57:28 -0400

    Updated resume page

--- portfolio-website/src/pages/Resume.js
+++ portfolio-website/src/pages/Resume.js
@@ -21,52 +21,46 @@ const Resume = () => {
 
     return (
         <div className="resume">
-            <div className="left-column">
-                <div className="resume-header">
-                    <div className="resume-header-name">
-                        <h1>Richard "Wylie" Glover</h1>
-                    </div>
-                    <div className="resume-header-email">
-                        <h3>(770) 508-7634</h3>
-                        <h3>rwylieg@gmail.com</h3>
-                    </div>
-                </div>
-                <div className="resume-content">
-                    <div className="skills">
-                        <h2>Skills</h2>
-                        <ul>
-                            <li>C/C++</li>
-                            <li>Java</li>
-                            <li>Python</li>
-                            <li>JavaScript</li>
-                            <li>HTML</li>
-                            <li>CSS</li>
-                        </ul>
-                    </div>
-                    <div className="technologies">
-                        <h2>Platforms</h2>
-                        <ul>
-                            <li>Django</li>
-                            <li>React.js</li>
-                            <li>Selenium</li>
-                            <li>Linux</li>
-                            <li>Ruby on Rails</li>
-                        </ul>
-                    </div>
+            <div className="resume-header">
+                <h1>Richard "Wylie" Glover</h1>
+                <div className="resume-header-email">
+                    <h3>(770) 508-7634</h3>
+                    <h3>rwylieg@gmail.com</h3>
                 </div>
             </div>
-            <div className="right-column">
-                <div className="image-container">
-                    <div className="download-button" onClick={handleDownload}>
-                        DOWNLOAD PDF
-                    </div>
-                    <img
-                        src={resume}
-                        alt="Resume"
-                        onClick={handleImageClick}
-                        className="download-image"
-                    />
+            <div className="resume-content">
+                <div className="skills">
+                    <h2>Skills</h2>
+                    <ul>
+                        <li>C/C++</li>
+                        <li>Java</li>
+                        <li>Python</li>
+                        <li>JavaScript</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                    </ul>
                 </div>
+                <div className="technologies">
+                    <h2>Platforms</h2>
+                    <ul>
+                        <li>Django</li>
+                        <li>React.js</li>
+                        <li>Selenium</li>
+                        <li>Linux</li>
+                        <li>Ruby on Rails</li>
+                    </ul>
+                </div>
+            </div>
+            <div className="image-container">
+                <button className="download-button" onClick={handleDownload}>
+                    DOWNLOAD PDF
+                </button>
+                <img
+                    src={resume}
+                    alt="Resume"
+                    onClick={handleImageClick}
+                    className="download-image"
+                />
             </div>
             {isBloomed && (
                 <div className="resume-modal" onClick={handleCloseModal}>
