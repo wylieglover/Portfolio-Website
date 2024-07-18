a2b44f80db1f67fd6bf277c8f8f165818fcb7896
wylieglover <rg11445@georgiasouthern.edu>
2024-07-17 20:38:47 -0400

    Updated home page

--- portfolio-website/src/components/Navbar.js
+++ portfolio-website/src/components/Navbar.js
@@ -5,11 +5,14 @@ import { Link } from "react-router-dom"; // Import Link component
 const Navbar = () => {
     return (
         <nav>
-            <ul className="navbar">
-                <li><Link to="/" className="nav-link">HOME</Link></li>
-                <li><Link to="/projects" className="nav-link">PROJECTS</Link></li>
-                <li><Link to="/resume" className="nav-link">RÉSUMÉ</Link></li>
-            </ul>
+            <div className="navbar-container">
+                <ul className="navbar">
+                    <li><Link to="/" className="nav-link">HOME</Link></li>
+                    <li><Link to="/projects" className="nav-link">PROJECTS</Link></li>
+                    <li><Link to="/resume" className="nav-link">RÉSUMÉ</Link></li>
+                </ul>
+            </div>
+
         </nav>
     );
 };
